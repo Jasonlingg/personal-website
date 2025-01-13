74baf6cf09ab8fef2680c08b683776836ff34462
Jason Ling <105073210+Jasonlingg@users.noreply.github.com>
2025-01-12 21:32:19 -0500

    Create About.tsx

---
+++ my-personal-website/src/components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div className="about">
+            <h2>About Me</h2>
+            <p>I'm a Systems Design Engineering student at the University of Waterloo, passionate about full-stack development and innovative technologies.</p>
+        </div>
+    );
+};
+
+export default About;
