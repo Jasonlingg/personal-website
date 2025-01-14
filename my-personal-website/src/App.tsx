8b94ea284ce8cbd2f3650463f2648c816fc8a2ff
Jason Ling <105073210+Jasonlingg@users.noreply.github.com>
2025-01-13 20:29:43 -0500

    fixing issues with dependencies

--- my-personal-website/src/App.tsx
+++ my-personal-website/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Education from './components/Education';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
@@ -15,12 +15,12 @@ const App: React.FC = () => {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" Component={Home} />
-          <Route path="/about" Component={About} />
-          <Route path="/skills" Component={Skills} />
-          <Route path="/experience" Component={Experience} />
-          <Route path="/education" Component={Education} />
-          <Route path="/contact" Component={Contact} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </div>
