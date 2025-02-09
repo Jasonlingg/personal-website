b34ca1b0aa2e08487f4d4f4d8b0080d4afa9ca07
Jason Ling <105073210+Jasonlingg@users.noreply.github.com>
2025-02-09 14:53:21 -0500

    update skills

--- my-personal-website/src/components/Skills.tsx
+++ my-personal-website/src/components/Skills.tsx
@@ -1,31 +1,37 @@
 import React from "react";
-import { Box, Text, Heading, Flex, Tooltip, Divider, Container, SimpleGrid } from "@chakra-ui/react";
+import {
+    Box,
+    Text,
+    Heading,
+    Flex,
+    Container,
+    SimpleGrid,
+    Avatar,
+    VStack,
+    Divider,
+    useColorModeValue,
+} from "@chakra-ui/react";
 import { motion } from "framer-motion";
-import { FaReact, FaHtml5, FaCss3Alt, FaJava, FaGithub, FaPython, FaNode, FaDocker, FaAws, FaLaptopCode } from "react-icons/fa";
-import { SiTypescript, SiJavascript, SiTailwindcss, SiSpringboot, SiNextdotjs, SiFlask, SiDjango, SiPostgresql, SiJira, SiConfluence, SiPostman } from "react-icons/si";
+import {
+    FaReact, FaHtml5, FaCss3Alt, FaJava, FaGithub, FaPython, FaNode, FaDocker, FaAws,
+} from "react-icons/fa";
+import {
+    SiTypescript, SiJavascript, SiTailwindcss, SiSpringboot, SiNextdotjs, SiFlask,
+    SiDjango, SiPostgresql, SiJira, SiConfluence, SiPostman, SiGooglecloud, SiVuedotjs,
+    SiReact, SiChakraui, SiMysql,
+} from "react-icons/si";
 import { TbBrandCpp } from "react-icons/tb";
+import meteor from "../animations/header.json";
+import Lottie from "lottie-react";
 
-// Define the type for the skill items
-interface Skill {
-    icon: React.ComponentType<any>;
-    name: string;
-    color: string;
-}
-interface Item {
-    icon: React.ElementType; // This expects a React component
-    name: string;
-    color: string;
-}
+const MotionBox = motion(Box);
+const MotionFlex = motion(Flex);
 
-// Define the type for the skills object
-interface Skills {
-    languages: Skill[];
-    frontend: Skill[];
-    backend: Skill[];
-    tools: Skill[];
-}
-
-const skills: Skills = {
+const fadeInVariant = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1.2, ease: "easeOut" } },
+};
+const skills = {
     languages: [
         { icon: FaJava, name: "Java", color: "#007396" },
         { icon: TbBrandCpp, name: "C++", color: "#00599C" },
@@ -34,11 +40,16 @@ const skills: Skills = {
         { icon: SiTypescript, name: "TypeScript", color: "#3178C6" },
         { icon: FaHtml5, name: "HTML", color: "#E34F26" },
         { icon: FaCss3Alt, name: "CSS", color: "#1572B6" },
+        { icon: SiMysql, name: "MySQL", color: "#4479A1" },
+
     ],
     frontend: [
         { icon: FaReact, name: "React", color: "#61DAFB" },
         { icon: SiNextdotjs, name: "Next.js", color: "#000000" },
+        { icon: SiVuedotjs, name: "Vue.js", color: "#42B883" },
+        { icon: SiReact, name: "React Native", color: "#61DAFB" },
         { icon: SiTailwindcss, name: "Tailwind", color: "#38B2AC" },
+        { icon: SiChakraui, name: "Chakra UI", color: "#319795" },
     ],
     backend: [
         { icon: SiFlask, name: "Flask", color: "#000000" },
@@ -46,6 +57,7 @@ const skills: Skills = {
         { icon: FaNode, name: "Node.js", color: "#339933" },
         { icon: SiDjango, name: "Django", color: "#092E20" },
         { icon: SiPostgresql, name: "PostgreSQL", color: "#336791" },
+        { icon: FaJava, name: "Java EE", color: "#007396" },
     ],
     tools: [
         { icon: FaGithub, name: "GitHub", color: "#181717" },
@@ -54,78 +66,117 @@ const skills: Skills = {
         { icon: SiPostman, name: "Postman", color: "#FF6C37" },
         { icon: FaDocker, name: "Docker", color: "#2496ED" },
         { icon: FaAws, name: "AWS", color: "#FF9900" },
+        { icon: SiGooglecloud, name: "GCP", color: "#4285F4" },
     ],
 };
 
 const containerVariant = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { staggerChildren: 0.15, delayChildren: 0.3 } },
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.6, staggerChildren: 0.2 } },
 };
 
 const itemVariant = {
-    hidden: { opacity: 0, scale: 0.8, y: 20 },
-    visible: { opacity: 1, scale: 1, y: 0, transition: { duration: 0.5, ease: "easeOut" } },
-    hover: { scale: 1.1, transition: { duration: 0.3, ease: "easeInOut" } },
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.3 } },
 };
 
-const Skills: React.FC = () => {
+const AboutSkills = () => {
     return (
-        <Box minH="100vh" w="full" position="relative" bg="rgb(217, 223, 230)"
-        >
-            <Container position="relative" maxW="6xl" py={10} bg='white' p={20} borderRadius="2xl" boxShadow="xl" zIndex="2">
-                <Heading as="h2" fontSize={{ base: '3xl', md: '4xl' }} textAlign="center" mb={6} color="blue.500">
-                    <Flex align="center" justify="center">
-                        <FaLaptopCode />
-                        <Text mx={5}>My Skills</Text>
-                        <FaLaptopCode />
-                    </Flex>
-                </Heading>
+        <Box bg="rgb(217, 223, 230)"
+            minH="100vh" py={10} display="flex" justifyContent="center">
 
-                {Object.entries(skills).map(([category, items]) => (
-                    <Box key={category} mb={8}>
-                        <Heading as="h3" fontSize={{ base: 'xl', md: 'lg' }} mb={4} textTransform="capitalize" textAlign="left" color="gray.700">
-                            {category}
+            {/* White container */}
+            <Container
+                maxW="5xl"
+                bg="rgb(217, 223, 230)" p={8}
+                // borderRadius="lg"
+                // boxShadow="2xl"
+                as={motion.div}
+                initial="hidden"
+                animate="visible"
+                variants={containerVariant}
+                zIndex={2}
+            >
+                {/* About Me Section */}
+                <Flex
+                    direction={{ base: "column", md: "row" }}
+                    align="center"
+                    justify="center"
+                    px={4}
+                    py={16}
+
+                    gap="80px"
+                >
+                    {/* Left Side - Lottie Animation */}
+                    <Box flex="1" maxW="300px" ml="-60px">
+                        <Lottie animationData={meteor} loop autoPlay />
+                    </Box>
+
+                    {/* Right Side - About Me Content */}
+                    <VStack spacing={5} textAlign="center" flex="2">
+                        <Heading size="lg" fontWeight="bold" color="gray.600">
+                            Nice To Meet You!
                         </Heading>
 
-                        <SimpleGrid
-                            columns={{ base: 2, sm: 3, md: 4, lg: 5 }}
-                            spacing={6}
-                            templateColumns="repeat(auto-fill, minmax(120px, 1fr))"
-                        >
-                            {items.map(({ icon: Icon, name, color }, index) => (
-                                <motion.div key={index} variants={itemVariant}>
-                                    <Tooltip label={name} hasArrow>
-                                        <Flex
-                                            as={motion.div}
-                                            whileHover={{ scale: 1.2, rotate: 5 }}
-                                            whileTap={{ scale: 0.9 }}
-                                            p={4}
-                                            bg="gray.100"
-                                            borderRadius="lg"
-                                            boxShadow="lg"
-                                            transition="all 0.3s"
-                                            _hover={{ bg: "white", boxShadow: "xl" }}
-                                            justify="center"
-                                            align="center"
-                                            fontSize="3xl"
-                                            color={color}
-                                            _focus={{ outline: "none", boxShadow: "0 0 0 3px rgba(66, 153, 225, 0.6)" }}
-                                        >
-                                            <Icon />
-                                            <Text fontSize="sm" fontWeight="bold" color="gray.700" ml="8px">
-                                                {name}
-                                            </Text>
-                                        </Flex>
-                                    </Tooltip>
-                                </motion.div>
-                            ))}
-                        </SimpleGrid>
-                        <Divider my={6} />
-                    </Box>
-                ))}
+                        <motion.div initial="hidden" animate="visible" variants={fadeInVariant}>
+                            <Text fontSize="lg" fontWeight="medium" textAlign="left">
+                                I have experience in both <Text as="span" fontWeight="bold">frontend and backend development</Text>, as well as <Text as="span" fontWeight="bold">mobile app development</Text>, working with modern frameworks like React TypeScript, Spring Boot, Django, and React Native.
+                                On the frontend, I focus on crafting <Text as="span" fontWeight="bold">responsive, accessible, and visually engaging</Text> user interfaces that enhance user interaction.
+                                On the backend, I specialize in building <Text as="span" fontWeight="bold">scalable APIs, optimizing database performance,</Text> and implementing <Text as="span" fontWeight="bold">secure authentication systems</Text> to support complex applications.
+                            </Text>
+                        </motion.div>
+
+                        <motion.div initial="hidden" animate="visible" variants={fadeInVariant}>
+                            <Text fontSize="lg" fontWeight="medium" textAlign="left">
+                                Outside of coding, I enjoy <Text as="span" fontWeight="bold">playing badminton</Text>—a sport that sharpens both my reflexes and strategic thinking.
+                                When I’m not on the court, you’ll often find me catching up on school, learning new cooking recipes, or watching sports on TV.
+                                I’m <Text as="span" fontWeight="bold">confident, naturally curious,</Text> and always striving to refine my skills.
+                            </Text>
+                        </motion.div>
+                    </VStack>
+                </Flex>
+
+                <Divider my={8} color="blue.500"></Divider>
+
+                {/* Skills Section */}
+                <Heading size="lg" textAlign="center" mb={6} color="gray.800">
+                    My Skills
+                </Heading>
+
+                <SimpleGrid columns={{ base: 2, md: 3 }} spacing={6}>
+                    {Object.entries(skills).map(([category, items]) => (
+                        <Box key={category}>
+                            <Heading size="md" color="gray.700" mb={4}>
+                                {category.charAt(0).toUpperCase() + category.slice(1)}
+                            </Heading>
+                            <Flex wrap="wrap" gap={4}>
+                                {items.map(({ icon: Icon, name, color }) => (
+                                    <MotionBox
+                                        key={name}
+                                        p={3}
+                                        borderRadius="md"
+                                        bg="gray.100"
+                                        boxShadow="md"
+                                        whileHover={{ scale: 1.1 }}
+                                        whileTap={{ scale: 0.9 }}
+                                        variants={itemVariant}
+                                        display="flex"
+                                        alignItems="center"
+                                        justifyContent="center"
+                                    >
+                                        <Icon size="30px" color={color} />
+                                        <Text ml={2} fontWeight="bold" color="gray.800">
+                                            {name}
+                                        </Text>
+                                    </MotionBox>
+                                ))}
+                            </Flex>
+                        </Box>
+                    ))}
+                </SimpleGrid>
             </Container>
-        </Box>
+        </Box >
     );
 };
 
-export default Skills;
+export default AboutSkills;
